6fab102975220dc13651100e35e7467197359a5b
reszcb740 <reszcb740@Gmail.com>
2023-01-07 06:43:05 +0000

    Add controller books users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    @user = current_user
+    @book = Book.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    @user = current_user
+    redirect_to user_path(current_user.id)
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: "homes#top"
+  resources :books
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
